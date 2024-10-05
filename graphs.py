fdf7e7c344c87ee5008e9ebc6875093b56d26c11
asur <gauravchauhan201700@gmail.com>
2024-10-06 03:32:01 +0530

    implement basic method like addEdge, addVertex, addEdge

---
+++ graphs.py
@@ -0,0 +1,49 @@
+class Graph:
+    def __init__(self, dict=None):
+        if dict is None:
+            self.dict = {}
+        self.dict = dict
+
+    def addEdge(self, vertex, edge):
+        self.dict[vertex].append(edge)
+
+    def addVertex(self, vertex):
+        if vertex not in self.dict.keys():
+            self.dict[vertex] = []
+        else:
+            print("vertex is already added")
+
+    def removeVertex(self, vertex):
+        if vertex not in self.dict.keys():
+            print("{} vertex is not in graph".format(vertex))
+        else:
+            for i in self.dict[vertex]:
+                self.dict[i].remove(vertex)
+            self.dict.pop(vertex)
+            print("successfully remove the vertex {}".format(vertex))
+
+    def removeEdge(self, vertexA, vertexB):
+        if vertexA not in self.dict.keys() or vertexB not in self.dict.keys():
+            print(" specified vertices is missing")
+        else:
+            self.dict[vertexA].remove(vertexB)
+            self.dict[vertexB].remove(vertexA)
+
+    def printGraph(self):
+        print(self.dict)
+
+customgraphdict = {
+    "A":["B", "C"],
+    "B":["D","E","A"],
+    "D":["E", "F","B"],
+    "F":["D","E"],
+    "E":["B","D","C"],
+    "C":["A","E"]
+}
+
+g = Graph(customgraphdict)
+g.addVertex("G")
+g.addEdge("G","D")
+g.addEdge("D", "G")
+g.removeEdge("A", "B")
+g.printGraph()
